627b7232e83cf0dcf02c576b2eb597c918948bf6
sergio <sergei.kigel@gmail.com>
2021-03-10 02:17:56 +0200

    fix verbosity in admin panel

--- urlShort/counter/models.py
+++ urlShort/counter/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 class counterModel(models.Model):
     count = models.IntegerField('count')
+
+    def __str__(self):
+        return 'Counter Entry'
+    
--- urlShort/urlShortApp/models.py
+++ urlShort/urlShortApp/models.py
@@ -5,4 +5,11 @@ class ShortUrl(models.Model):
     fullUrl = models.URLField(max_length=400)
     hashedUrl = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published',auto_now=True)
+
+    class Meta:
+        verbose_name = 'URL Entry'
+        verbose_name_plural = 'URL Entries'
+
+    def __str__(self):
+        return 'Entry {}'.format(self.hashedUrl)
     
