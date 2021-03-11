6dbc6fc0543c4b57aa31276ac0b1784cd5409dd1
sergio <sergei.kigel@gmail.com>
2021-03-11 15:24:26 +0200

    remove base url

--- urlShort/urlShort/urls.py
+++ urlShort/urlShort/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('x/', include('urlShortApp.urls')),
     path('count/', include('counter.urls')),
     path('admin/', admin.site.urls),
+    path('', include('urlShortApp.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
