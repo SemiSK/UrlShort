7bb85ecf274db0ea6b8dcd6f6336b38c83ccf676
sergio <sergei.kigel@gmail.com>
2021-03-13 12:40:16 +0200

    fix case of no count object

--- urlShort/counter/views.py
+++ urlShort/counter/views.py
@@ -6,6 +6,9 @@ from .models import counterModel
 
 def counter_view(request):
     count_object = counterModel.objects.last()
+    if count_object is None:
+        counterModel(count=0).save()
+        count_object = counterModel.objects.last()
     count = count_object.count
     count += 1
     count_object.count = count
